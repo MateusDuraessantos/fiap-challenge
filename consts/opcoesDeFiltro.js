0fe180e1af5691d1535c8fa89d64a87bfd1fa8c2
MateusDuraessantos <mateusduraessantos@gmail.com>
2025-10-03 19:23:31 -0300

    responsividade

---
+++ consts/opcoesDeFiltro.js
@@ -0,0 +1,106 @@
+
+export const opcoesDeFiltroProdutos = [
+  {
+    titulo: 'ORDENAR',
+    opcoes: [
+      'MAIOR PREÇO',
+      'MAIOR DESCONTO',
+      'MENOR PREÇO',
+      'A a Z',
+      'MAIS VENDIDO',
+      'Z a A',
+    ]
+  },
+  {
+    titulo: 'LINHAS SWIFT',
+    opcoes: [
+      'ARGENTINO',
+      'ARGENTINO',
+      'BLACK',
+      'BLACK',
+      'BRONZE',
+      'BRONZE',
+      'GRAN RESERVA',
+      'GRAN RESERVA',
+      ]
+  },
+  {
+    titulo: 'CATEGORIAS',
+    opcoes: [
+      'ACÉM  (7)',
+      'ACÉM  (7)',
+      'ALCATRA (6)',
+      'ALCATRA (6)',
+      'ANCHO  (9)',
+      'ANCHO  (9)',
+      'ASSADO DE TIRAS (3)',
+      'ASSADO DE TIRAS (3)'
+    ],               
+  },
+  {
+    titulo: 'FORMATOS',
+    opcoes: [
+      'BIFES',
+      'BIFES',
+      'CARPACCIO',
+      'CARPACCIO',
+      'COSTELA',
+      'COSTELA',
+      'CUBOS',
+      'CUBOS',
+      ],
+  },
+  {
+    titulo: 'CORTES',
+    opcoes: [
+      'PALETA',
+      'PALETA',
+      'PATINHO',
+      'PATINHO',
+      'PEITO',
+      'PEITO',
+      'PRIME RIB',
+      'PRIME RIB',
+    ]
+  }
+]
+
+export const opcoesDeFiltroProdutosReceitas = [
+  {
+    titulo: 'DIFICULDADE',
+    opcoes: [
+      'MAIOR PREÇO',
+      'MAIOR DESCONTO',
+      'MENOR PREÇO',
+      'A a Z',
+      'MAIS VENDIDO',
+      'Z a A',
+    ],               
+  },
+  {
+    titulo: 'TIPO',
+    opcoes: [
+      'ACÉM  (7)', 
+      'ACÉM  (7)', 
+      'ALCATRA (6)', 
+      'ALCATRA (6)', 
+      'ANCHO  (9)', 
+      'ANCHO  (9)', 
+      'ASSADO DE TIRAS (3)',
+      'ASSADO DE TIRAS (3)',
+      ],
+  },
+  {
+    titulo: 'OCASIÕES',
+    opcoes: [
+      'BIFES',
+      'BIFES',
+      'CARPACCIO',
+      'CARPACCIO',
+      'COSTELA',
+      'COSTELA',
+      'CUBOS',
+      'CUBOS',
+    ]
+  }
+]
